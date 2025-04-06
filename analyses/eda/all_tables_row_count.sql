719b34b047d181008cc6e48d1b0c4a5ebb82a054
nam-97 <nam.nx@joonsolutions.com>
2025-04-06 16:00:27 +0800

    add path for staging in dbt project

--- analyses/eda/all_tables_row_count.sql
+++ analyses/eda/all_tables_row_count.sql
@@ -1,11 +1,19 @@
-select 'account_created' as table_source, count(*) as total_rows
+select
+    'account_created' as table_source,
+    count(*) as total_rows
 from analytics-take-home-test.monzo_datawarehouse.account_created
 union all
-select 'account_closed' as table_source, count(*) as total_rows
+select
+    'account_closed' as table_source,
+    count(*) as total_rows
 from analytics-take-home-test.monzo_datawarehouse.account_closed
 union all
-select 'account_reopened' as table_source, count(*) as total_rows
+select
+    'account_reopened' as table_source,
+    count(*) as total_rows
 from analytics-take-home-test.monzo_datawarehouse.account_reopened
 union all
-select 'account_transactions' as table_source, count(*) as total_rows
+select
+    'account_transactions' as table_source,
+    count(*) as total_rows
 from analytics-take-home-test.monzo_datawarehouse.account_transactions
